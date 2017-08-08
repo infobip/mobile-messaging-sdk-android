731af53dbd093a1a08de35b0cd3c51a2ea4eae31
Andrey Averyanov <Andrey.Averyanov@infobip.com>
2017-08-08 09:59:23 +0200

    Merge pull request #108 in MML/infobip-mobile-messaging-android from aaveryanov-MM-1985-web-view-in-example-app to master
    Squashed commit of the following:
    
    commit 24e03b5df7fd8677b26afaf03552534d6d3deda5
    Author: aaveryanov <andrey.averyanov@infobip.com>
    Date:   Mon Aug 7 19:51:32 2017 +0300
    
        pr fixes
    
    commit ec33fd8868c8caf427eb307aaeb6492d5e2b4753
    Author: aaveryanov <andrey.averyanov@infobip.com>
    Date:   Mon Aug 7 19:47:24 2017 +0300
    
        pr fixes
    
    commit f59ee63547a7c7a0dbb9e38437d64bda6ede0573
    Author: aaveryanov <andrey.averyanov@infobip.com>
    Date:   Mon Aug 7 19:43:22 2017 +0300
    
        remove namespace from children view
    
    commit 8ee333c8571e8556201c4ab1be5f22b076e02d5b
    Author: aaveryanov <andrey.averyanov@infobip.com>
    Date:   Mon Aug 7 19:37:15 2017 +0300
    
        move "url" param in constant, change fill_parent to match_parent, align layouts
    
    commit 0e51f9e77a1002ec680444557bbbb138c72a91b5
    Author: aaveryanov <andrey.averyanov@infobip.com>
    Date:   Mon Aug 7 19:20:18 2017 +0300
    
        application style back
    
    commit 82e7f4ee44eb8f19a579dea949cc352cfef5e98b
    Author: aaveryanov <andrey.averyanov@infobip.com>
    Date:   Mon Aug 7 19:13:28 2017 +0300
    
        enable js in web view
    
    commit 276d194f8f2a651158b83ea8f83c8d102b3845e4
    Author: aaveryanov <andrey.averyanov@infobip.com>
    Date:   Mon Aug 7 18:46:10 2017 +0300
    
        delete app code and sender id
    
    commit 3eba806eeac1812c3f96fc1725bfac0232a9b043
    Author: aaveryanov <andrey.averyanov@infobip.com>
    Date:   Mon Aug 7 18:43:22 2017 +0300
    
        use AppCompatActivity as base class for webView, put main activity as starting point back, move toolbar to separate layout
    
    commit eb5b86a9052103a5bb707c8e538610c7f5c97bd4
    Author: aaveryanov <andrey.averyanov@infobip.com>
    Date:   Mon Aug 7 16:32:23 2017 +0300
    
        add web view activity (as first screen)

--- infobip-mobile-messaging-android-demo/src/main/java/org/infobip/mobile/messaging/demo/MainActivity.java
+++ infobip-mobile-messaging-android-demo/src/main/java/org/infobip/mobile/messaging/demo/MainActivity.java
@@ -42,6 +42,7 @@ import org.infobip.mobile.messaging.geo.MobileGeo;
 import org.infobip.mobile.messaging.mobile.MobileMessagingError;
 import org.infobip.mobile.messaging.storage.SQLiteMessageStore;
 import org.infobip.mobile.messaging.util.StringUtils;
+import org.json.JSONException;
 
 import java.util.Locale;
 
@@ -115,6 +116,17 @@ public class MainActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             Message message = Message.createFrom(intent.getExtras());
             String body = message.getBody();
+            String url = null;
+
+            try {
+                url = message.getCustomPayload().getString("url");
+            } catch (JSONException ignored) {
+            }
+
+            if (url != null) {
+                openWebView(url);
+            }
+
             Toast.makeText(MainActivity.this, String.format(Locale.getDefault(), getString(R.string.toast_notification_tapped), body), Toast.LENGTH_LONG).show();
             updateCount();
         }
@@ -422,5 +434,12 @@ public class MainActivity extends AppCompatActivity {
                 .edit()
                 .putString(ApplicationPreferences.MSISDN, "" + userData.getMsisdn())
                 .apply();
+    }
+
+    private void openWebView(String url) {
+        Intent webViewIntent = new Intent(this, WebViewActivity.class);
+        webViewIntent.putExtra(WebViewActivity.EXTRA_URL, url);
+
+        startActivity(webViewIntent);
     }
 }
