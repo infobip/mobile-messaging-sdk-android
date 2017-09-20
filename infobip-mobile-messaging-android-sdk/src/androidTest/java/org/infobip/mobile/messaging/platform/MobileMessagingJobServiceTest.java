bb8e344081c6914e4ea1b8831c44fb3379bf65e7
Stanislav Slavin <stanislav.slavin@infobip.com>
2017-09-20 10:00:11 +0300

    Better handling of job parameters in JobService test

--- infobip-mobile-messaging-android-sdk/src/androidTest/java/org/infobip/mobile/messaging/platform/MobileMessagingJobServiceTest.java
+++ infobip-mobile-messaging-android-sdk/src/androidTest/java/org/infobip/mobile/messaging/platform/MobileMessagingJobServiceTest.java
@@ -3,16 +3,12 @@ package org.infobip.mobile.messaging.platform;
 import android.app.job.JobParameters;
 import android.app.job.JobScheduler;
 import android.content.Context;
-import android.net.Uri;
-import android.os.PersistableBundle;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.infobip.mobile.messaging.MobileMessagingCore;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.lang.reflect.InvocationTargetException;
 
 import static org.infobip.mobile.messaging.platform.MobileMessagingJobService.ON_NETWORK_AVAILABLE_ID;
 import static org.mockito.Matchers.eq;
@@ -32,43 +28,26 @@ import static org.mockito.Mockito.verify;
 public class MobileMessagingJobServiceTest {
 
     private MobileMessagingCore mmcMock = mock(MobileMessagingCore.class);
-    private JobScheduler jobScheduler = mock(JobScheduler.class);
+    private JobScheduler givenJobScheduler = mock(JobScheduler.class);
+    private JobParameters givenJobParameters = mock(JobParameters.class);
 
     @After
     public void after() {
-        reset(jobScheduler, mmcMock);
+        reset(givenJobScheduler, mmcMock);
     }
 
     @Test
     public void shouldInvokeResyncWhenJobStarts() throws Exception {
         // Given
         MobileMessagingJobService givenJobService = spy(new MobileMessagingJobService(mmcMock));
-        doReturn(jobScheduler).when(givenJobService).getSystemService(eq(Context.JOB_SCHEDULER_SERVICE));
+        doReturn(givenJobScheduler).when(givenJobService).getSystemService(eq(Context.JOB_SCHEDULER_SERVICE));
         doReturn(getClass().getPackage().getName()).when(givenJobService).getPackageName();
+        doReturn(ON_NETWORK_AVAILABLE_ID).when(givenJobParameters).getJobId();
 
         // When
-        givenJobService.onStartJob(givenJob(ON_NETWORK_AVAILABLE_ID));
+        givenJobService.onStartJob(givenJobParameters);
 
         // Then
         verify(mmcMock, times(1)).retrySync();
     }
-
-    //region private methods
-    /**
-     * Creates job parameters in a hacky way through reflection.
-     * <p/>
-     * Because constructor for {@link JobParameters} is not publically available.
-     *
-     * @return job parameters
-     */
-    private JobParameters givenJob(int jobId) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        try {
-            return (JobParameters) JobParameters.class.getConstructors()[0]
-                    .newInstance(null, jobId, new PersistableBundle(), false, new Uri[0], new String[0]);
-        } catch (IllegalArgumentException ignored) {
-            return (JobParameters) JobParameters.class.getConstructors()[0]
-                    .newInstance(null, jobId, new PersistableBundle(), false);
-        }
-    }
-    //endregion
 }
