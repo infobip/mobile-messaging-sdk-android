1c0f002d33a77eb8fe046853135b9bbb70939d95
Olga Koroleva <Olga.Koroleva@infobip.com>
2020-12-24 17:31:17 +0300

    enhancement for java docs for in-app chat

--- infobip-mobile-messaging-android-chat-sdk/src/main/java/org/infobip/mobile/messaging/chat/view/InAppChatFragment.java
+++ infobip-mobile-messaging-android-chat-sdk/src/main/java/org/infobip/mobile/messaging/chat/view/InAppChatFragment.java
@@ -25,9 +25,9 @@ import android.provider.MediaStore;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.graphics.ColorUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
@@ -131,11 +131,9 @@ public class InAppChatFragment extends Fragment implements InAppChatWebViewManag
         /**
          * Implement back button behaviour.
          * <br>
-         * Call following method with corresponding parameters:
-         * <pre>
-         * {@code
-         * InAppChat.getInstance(Context context).openChatFragment(FragmentManager fragmentManager, int containerId)
-         * </pre>
+         * Call following method with corresponding parameter:
+         * <br>
+         * {@link InAppChat#hideInAppChatFragment(FragmentManager)}
          */
         void onInAppChatBackPressed();
     }
