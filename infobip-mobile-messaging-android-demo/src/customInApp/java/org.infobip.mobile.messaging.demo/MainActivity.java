2a3588ce5367f1b85766c2363577dd637e81dbe2
ikresic <ivan.kresic@infobip.com>
2023-07-21 15:39:51 +0200

    - cleanup

--- infobip-mobile-messaging-android-demo/src/customInApp/java/org.infobip.mobile.messaging.demo/MainActivity.java
+++ infobip-mobile-messaging-android-demo/src/customInApp/java/org.infobip.mobile.messaging.demo/MainActivity.java
@@ -2,17 +2,9 @@ package org.infobip.mobile.messaging.demo;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import org.infobip.mobile.messaging.Message;
-import org.infobip.mobile.messaging.platform.Platform;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
