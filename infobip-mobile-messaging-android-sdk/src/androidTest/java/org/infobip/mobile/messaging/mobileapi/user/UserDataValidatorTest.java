ed8cb79075cb0f3d7a97a4ece68caa936161ec16
Ivan Krešić <Ivan.Kresic@infobip.com>
2025-11-07 10:16:24 +0000

    Pull request #542: Ikresic MM-7234 android sdk add field validation
    Merge in MML/infobip-mobile-messaging-android from ikresic-MM-7234-android-sdk-add-field-validation to master
    
    Squashed commit of the following:
    
    commit 065810d9bca4944eac8061a451fecb39088da483
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Fri Nov 7 10:55:57 2025 +0100
    
        - fixed PR comments
    
    commit 1c7d0450689cfe79c68e3a6f0e7b173aedd7d5dc
    Merge: 4623f1138 172402dc9
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Fri Nov 7 10:48:05 2025 +0100
    
        Merge branch 'master' into ikresic-MM-7234-android-sdk-add-field-validation
    
    commit 4623f113841b6f44285e2918bc804cf56e87755e
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Thu Nov 6 15:10:06 2025 +0100
    
        - fixed PR comments
    
    commit afe8c93102b9e264bb9ca50932acd66cd840b63e
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Thu Nov 6 14:31:18 2025 +0100
    
        - fixed PR comments
    
    commit 2e8f30c1a7ec8e02e08d5a54df6244286cfbc3f0
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Thu Nov 6 14:09:37 2025 +0100
    
        - fixed PR comments
    
    commit 442e7f509fa3c5cfea105d47c9ad4f4338fa4494
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Thu Nov 6 13:56:26 2025 +0100
    
        - fixed PR comments
    
    commit 9cc5516215e4632b1b99edf3ee12d5b1739fc63c
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Thu Nov 6 13:50:43 2025 +0100
    
        - fixed PR comments
    
    commit ed16f3b6425018a8cdadb25dcd24f7c2bb61cddc
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Thu Nov 6 13:19:41 2025 +0100
    
        - fixed PR comments
    
    commit 095cb3fa543573d8a1caa6370fc1db0b017560e9
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Wed Nov 5 16:25:42 2025 +0100
    
        - fixed PR comments
    
    commit edd8aae44fdcc06a1d1165bdb00103b90525d348
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Fri Oct 24 14:31:22 2025 +0200
    
        - provided validation for a user
    
    commit addbc3ec5048437ad996ee41ef97f7b5b28e621d
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Fri Oct 24 14:30:27 2025 +0200
    
        - provided validation for a user
    
    commit 7be4024ceb8a01c7d5a89574661fba57f23f4db4
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Fri Oct 24 14:26:47 2025 +0200
    
        - provided validation for a user
    
    commit 93f2245a93096757a84259dff7f52a6a5e9efa1f
    Author: Ivan Kresic <ivan.kresic@infobip.com>
    Date:   Fri Oct 24 14:24:40 2025 +0200
    
        - provided validation for a user

--- infobip-mobile-messaging-android-sdk/src/androidTest/java/org/infobip/mobile/messaging/mobileapi/user/UserDataValidatorTest.java
+++ infobip-mobile-messaging-android-sdk/src/androidTest/java/org/infobip/mobile/messaging/mobileapi/user/UserDataValidatorTest.java
@@ -1,3 +1,10 @@
+/*
+ * UserDataValidatorTest.java
+ * Mobile Messaging SDK
+ *
+ * Copyright (c) 2016-2025 Infobip Limited
+ * Licensed under the Apache License, Version 2.0
+ */
 package org.infobip.mobile.messaging.mobileapi.user;
 
 import static org.junit.Assert.assertNotNull;
